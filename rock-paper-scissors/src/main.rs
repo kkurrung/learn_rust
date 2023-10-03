1cfc5a55367c5883b1fdac4263e5765b26c76ada
kkurrung <kkurrung@gmail.com>
2023-10-03 11:51:34 +0900

    새 폴더

---
+++ rock-paper-scissors/src/main.rs
@@ -0,0 +1,10 @@
+
+use std::io;
+
+fn main() {
+    println!("[가위, 바위, 보]중 하나를 입력 해 주세요.");
+
+    let mut decision = String::new();
+    io::stdin().read_line(&mut decision).expect("입력 실패");
+    println!("당신의 선택은 {decision} 입니다.");
+}
